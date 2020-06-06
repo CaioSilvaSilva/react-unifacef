86684a30275e0b5f333aacd09a1dce07507abd99
Caio Poli <caio.poli@ernetwork.com.br>
2020-06-06 11:45:06 -0300

    :sparkles: Adding mobx

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import * as Sentry from '@sentry/browser';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'mobx-react';
+import { createBrowserHistory } from 'history';
+import { Router } from 'react-router-dom';
+import Routes from './routes';
+import { syncHistoryWithStore } from 'mobx-react-router';
+
+import { router } from './mobx';
+import * as store from './mobx';
 
 Sentry.init({dsn: "https://c382da9c98444a388033153b127978fe@o403751.ingest.sentry.io/5266734"});
 
+const rootElement = document.getElementById('root');
+const browserHistory = createBrowserHistory();
+const history = syncHistoryWithStore(browserHistory, router);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider {...store}>
+      <Router history={history}>
+        <Routes />
+      </Router>
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  rootElement
 );
 
 // If you want your app to work offline and load faster, you can change
