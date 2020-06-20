f65653fed870b0070f51d9d6dcd4a8886184ea08
Caio Poli <caio.poli@ernetwork.com.br>
2020-06-20 11:08:47 -0300

    :sparkles: Adding route star wars details

--- src/routes/endpoints.ts
+++ src/routes/endpoints.ts
@@ -3,6 +3,7 @@ import Home from '../containers/home';
 import { RouteProps } from 'react-router-dom';
 import Sobre from '../containers/sobre';
 import StarWars from '../containers/star-wars';
+import StarWarsDetails from '../containers/star-wars-details';
 
 const publicUrl = process.env.PUBLIC_URL;
 
@@ -16,4 +17,5 @@ export const endpoints: EndPointsProps[] = [
   { path: `${publicUrl}/sobre`, name: 'Sobre', component: Sobre, exact: true },
   { path: `${publicUrl}/combustivel`, name: 'Combustível', component: Combustivel, exact: true },
   { path: `${publicUrl}/star-wars`, name: 'Star Wars', component: StarWars, exact: true },
+  { path: `${publicUrl}/star-wars/:id`, component: StarWarsDetails, exact: true },
 ];
